3ba9db02d1f0ef27d22a40bebae7e2e3afcf2441
Josh Bartow <jbartow92@gmail.com>
2023-08-23 15:55:47 -0500

    eateries list update

---
+++ scripts/eateries/EateryProvider.js
@@ -0,0 +1,17 @@
+export const displayEateriesList = async () => {
+    const response = await fetch("http://holidayroad.nss.team/eateries");
+    const eateries = await response.json();
+  
+    let html = `<select class="dropdown" id="eateries_dropdown">
+    <option value="0">Tasty Places</option>`;
+    const divStringArray = eateries.map((eat) => {
+        if (eat.ameneties.wheelchairAccessible == "true") {
+
+            return `
+            <option value=${eat.id}> ${eat.businessName}`;
+        }
+    });
+    html += divStringArray.join("");
+    html += `</select>`;
+    return html;
+  };
