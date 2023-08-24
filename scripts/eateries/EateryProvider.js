e1e8587322969c12fdb772049c3f7ba3eb4c32c2
Josh Bartow <jbartow92@gmail.com>
2023-08-24 09:11:37 -0500

    made dropdown for eateries

--- scripts/eateries/EateryProvider.js
+++ scripts/eateries/EateryProvider.js
@@ -2,10 +2,10 @@ export const displayEateriesList = async () => {
     const response = await fetch("http://holidayroad.nss.team/eateries");
     const eateries = await response.json();
   
-    let html = `<select class="dropdown" id="eateries_dropdown">
+    let html = `<select class="eatery" id="eateries_dropdown">
     <option value="0">Tasty Places</option>`;
     const divStringArray = eateries.map((eat) => {
-        if (eat.ameneties.wheelchairAccessible == "true") {
+        if (eat.ameneties.wheelchairAccessible === true) {
 
             return `
             <option value=${eat.id}> ${eat.businessName}`;
