092883b04a64e8ad505bf3a621cfa041187190c5
Josh Bartow <jbartow92@gmail.com>
2023-08-24 15:08:55 -0500

    created event listener for eats and attractions

--- scripts/eateries/EateryProvider.js
+++ scripts/eateries/EateryProvider.js
@@ -6,12 +6,31 @@ export const displayEateriesList = async () => {
     <option value="0">Tasty Eatery Options...</option>`;
     const divStringArray = eateries.map((eat) => {
         if (eat.ameneties.wheelchairAccessible === true) {
-
             return `
-            <option value=${eat.id}> ${eat.businessName}`;
+            <option data-eats="${eat.businessName}" value=${eat.id}> ${eat.businessName}`;
         }
-    });
+            
+        });
     html += divStringArray.join("");
     html += `</select>`;
     return html;
   };
+
+  document.addEventListener("change", (changeEvent) => {
+    const selectElement = changeEvent.target;
+  
+    if (selectElement.dataset.type === "eatery") {
+      const selectedOption = selectElement.options[selectElement.selectedIndex];
+      const eateryName = selectedOption.dataset.eats;
+  
+      let detailsHtml = `
+        <div>
+          <h4>Eatery Preview</h4>
+          <p>${eateryName}</p>
+          <div></div>
+        </div>`;
+  
+      const parentTag = document.querySelector(".Preview_eat");
+      parentTag.innerHTML = detailsHtml;
+    }
+  });
